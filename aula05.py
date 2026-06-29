fb7feb3a3344c53da2e53bf79298aa72d0194449
Yasmin Araujo <yasminju07@gmail.com>
2026-06-29 10:54:58 -0300

    Create aula05.py
    Um algorítmo que leia um número e mostre o seu dobro, triplo e raiz quadrada

---
+++ aula05.py
@@ -0,0 +1,7 @@
+a = int(input('Digite um valor: '))
+d = a * 2
+t + a * 3
+r + a** (1/2)
+print('O dobro de {} vale {}'.format(a,d))
+print('O triplo de {} vale {}'.format(a,t))
+print('A raiz quadrada de {} vale {}'.format(a,r))
