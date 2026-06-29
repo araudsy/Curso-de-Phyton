873f67a9ebf8cf25506308fa037dc0aef1ef5883
Yasmin Araujo <yasminju07@gmail.com>
2026-06-29 11:13:21 -0300

    Create aula08.py
    Um programa que leia um número inteiro qualquer e mostre na tela a tabuada.

---
+++ aula08.py
@@ -0,0 +1,23 @@
+n = int(input('Digite um número para ver sua tabuada: '))
+t1 = n * 1
+t2 = n * 2
+t3 = n * 3
+t4 = n * 4
+t5 = n * 5
+t6 = n * 6
+t7 = n * 7
+t8 = n * 8
+t9 = n * 9
+t10 = n * 10
+print('-'* 12)
+print(f'{n} x 1 = {n*1}')
+print(f'{n} x 2 = {n*2}')
+print(f'{n} x 3 = {n*3}')
+print(f'{n} x 4 = {n*4}')
+print(f'{n} x 5 = {n*5}')
+print(f'{n} x 6 = {n*6}')
+print(f'{n} x 7 = {n*7}')
+print(f'{n} x 8 = {n*8}')
+print(f'{n} x 9 = {n*9}')
+print(f'{n} x 10 = {n*10}')
+print('-'* 12)
